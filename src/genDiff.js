81ab7d17f82fd385c9cca0324b9c84b702e6592e
ivankl <ivan@roonlabs.com>
2020-03-01 23:05:40 +0300

    changes for Comparing functionality

--- src/genDiff.js
+++ src/genDiff.js
@@ -20,20 +20,22 @@ export const getAllKeyes = (object1, object2) => {
   return keysOfObject1.concat(newKeysInObject2);
 };
 
+const getDiffForCurrentKey = (object1, object2, item) => {
+  if (_.has(object2, item)) {
+    if (object2[item] === object1[item]) {
+      return `${item}: ${object1[item]}\n`;
+    }
+    if (!(_.has(object1, item))) {
+      return `+ ${item}: ${object2[item]}\n`;
+    }
+    return `+ ${item}: ${object2[item]}\n- ${item}: ${object1[item]}\n`;
+  }
+  return `- ${item}: ${object1[item]}\n`;
+};
+
 export const compareObjects = (object1, object2) => {
   const allUsedKeys = getAllKeyes(object1, object2);
-  const result = allUsedKeys.reduce((acc, item) => {
-    if (_.has(object2, item)) {
-      if (object2[item] === object1[item]) {
-        return `${acc}${item}: ${object1[item]}\n`;
-      }
-      if (!(_.has(object1, item))) {
-        return `${acc}+ ${item}: ${object2[item]}\n`;
-      }
-      return `${acc}+ ${item}: ${object2[item]}\n- ${item}: ${object1[item]}\n`;
-    }
-    return `${acc}- ${item}: ${object1[item]}\n`;
-  }, '');
+  const result = allUsedKeys.reduce((acc, key) => `${acc}${getDiffForCurrentKey(object1, object2, key)}`, '');
   return result;
 };
 
