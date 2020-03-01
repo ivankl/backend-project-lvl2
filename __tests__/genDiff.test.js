da4d1763f6151b88ef5cb18af56f5a755007b56f
ivankl <ivan@roonlabs.com>
2020-03-01 23:58:57 +0300

    improved test coverage

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,6 +1,6 @@
 import path from 'path';
 import fs from 'fs';
-import { resolveFilePath, getParsedData, compareObjects } from '../src/genDiff';
+import { resolveFilePath, getParsedData, genJSONDiff } from '../src/genDiff';
 
 const object = {
   name: 'backend-project-lvl2',
@@ -11,14 +11,11 @@ const object = {
 
 const fixturesPath = '__fixtures__';
 
-const constructFullPath = (filename) => {
-  console.log(`${path.resolve(`${__dirname}`, '..')}`);
-  return `${path.resolve(`${__dirname}`, `../${fixturesPath}`)}/${filename}`;
-};
+const constructFullPath = (filename) => `${path.resolve(`${__dirname}`, `../${fixturesPath}`)}/${filename}`;
 
 
 test('Can a file with relative path be parsed', () => {
-  const pathResult = resolveFilePath('__fixtures__/before.json');
+  const pathResult = resolveFilePath(`${fixturesPath}/before.json`);
   const expectedPath = constructFullPath('before.json');
   expect(pathResult).toEqual(expectedPath);
   const object1 = getParsedData(pathResult);
@@ -36,16 +33,10 @@ test('Can a file with absolute path be parsed', () => {
 });
 
 test('Compare objects', () => {
-  const pathResult1 = resolveFilePath('__fixtures__/before.json');
-  const pathResult2 = resolveFilePath('__fixtures__/after.json');
-  const fullPathToBefore = constructFullPath('before.json');
-  const fullPathToAfter = constructFullPath('after.json');
+  const fullPathToFile1 = constructFullPath('before.json');
+  const fullPathToFile2 = constructFullPath('after.json');
   const fullPathToResult = constructFullPath('diff.txt');
-  expect(pathResult1).toEqual(fullPathToBefore);
-  expect(pathResult2).toEqual(fullPathToAfter);
-  const object1 = getParsedData(pathResult1);
-  const object2 = getParsedData(pathResult2);
   const expectedResult = fs.readFileSync(fullPathToResult, 'utf-8');
-  const compareResult = compareObjects(object1, object2);
+  const compareResult = genJSONDiff(fullPathToFile1, fullPathToFile2);
   expect(compareResult).toEqual(expectedResult);
 });
