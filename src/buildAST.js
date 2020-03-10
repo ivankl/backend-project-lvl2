1f3ebe8fbdc998324a32eff437f1b9036094810f
ivankl <ivan@roonlabs.com>
2020-03-11 00:52:00 +0300

    some refactoring changes to buildAST.js

--- src/buildAST.js
+++ src/buildAST.js
@@ -3,52 +3,33 @@ import { areValuesEqual, areBothItemsArrays, areBothItemsObjects } from './utils
 
 const getAllKeyes = (object1, object2) => _.union(Object.keys(object1), Object.keys(object2));
 
-export const buildAST = (object1, object2, depth = 1) => {
+const buildNode = (object1, object2, key, fn) => {
+  if (!(areBothItemsArrays(object1[key], object2[key]))) {
+    if (areBothItemsObjects(object1[key], object2[key])) {
+      return { key, type: 'nested', value: fn(object1[key], object2[key]) };
+    }
+  }
+  if (areValuesEqual(object1[key], object2[key])) {
+    return { key, type: 'unchanged', value: object1[key] };
+  }
+  if (!(_.has(object1, key)) && _.has(object2, key)) {
+    return { key, type: 'added', value: object2[key] };
+  }
+  if (!(_.has(object2, key)) && _.has(object1, key)) {
+    return { key, type: 'removed', value: object1[key] };
+  }
+  return {
+    key,
+    type: 'modified',
+    oldValue: object1[key],
+    newValue: object2[key],
+  };
+};
+
+export const buildAST = (object1, object2) => {
   const allUsedKeys = getAllKeyes(object1, object2);
-  const result = allUsedKeys.reduce((acc, item) => {
-    if (!(areBothItemsArrays(object1[item], object2[item]))) {
-      if (areBothItemsObjects(object1[item], object2[item])) {
-        return [...acc, {
-          key: item,
-          type: 'nested',
-          depth,
-          value: buildAST(object1[item], object2[item], depth + 1),
-        }];
-      }
-    }
-    if (areValuesEqual(object1[item], object2[item])) {
-      return [...acc, {
-        key: item,
-        type: 'unchanged',
-        depth,
-        value: object1[item],
-      }];
-    }
-    if (!(_.has(object1, item)) && _.has(object2, item)) {
-      return [...acc, {
-        key: item,
-        type: 'added',
-        depth,
-        value: object2[item],
-      }];
-    }
-    if (!(_.has(object2, item)) && _.has(object1, item)) {
-      return [...acc, {
-        key: item,
-        type: 'removed',
-        depth,
-        value: object1[item],
-      }];
-    }
-    return [...acc, {
-      key: item,
-      type: 'modified',
-      depth,
-      oldValue: object1[item],
-      newValue: object2[item],
-    }];
-  }, []);
-  return result;
+  return allUsedKeys.reduce((acc, key) => [...acc, buildNode(object1, object2, key, buildAST)],
+    []);
 };
 
 export default buildAST;
--- src/renderers/render.js
+++ src/renderers/render.js
@@ -2,19 +2,19 @@ import valueRender from './valueRenderer';
 import { addSpaces } from '../utils';
 
 const renderTypeDispatch = {
-  unchanged: (item) => `  ${item.key}: ${valueRender(item.value, item.depth)}`,
-  removed: (item) => `- ${item.key}: ${valueRender(item.value, item.depth)}`,
-  added: (item) => `+ ${item.key}: ${valueRender(item.value, item.depth)}`,
-  modified: (item) => `+ ${item.key}: ${valueRender(item.newValue, item.depth)}${addSpaces(item.depth)}- ${item.key}: ${valueRender(item.oldValue, item.depth)}`,
+  unchanged: (item, depth) => `  ${item.key}: ${valueRender(item.value, depth)}`,
+  removed: (item, depth) => `- ${item.key}: ${valueRender(item.value, depth)}`,
+  added: (item, depth) => `+ ${item.key}: ${valueRender(item.value, depth)}`,
+  modified: (item, depth) => `+ ${item.key}: ${valueRender(item.newValue, depth)}${addSpaces(depth)}- ${item.key}: ${valueRender(item.oldValue, depth)}`,
 };
 
-export const render = (ast) => {
-  const adjustDepthForBrackets = ast[0].depth - 0.5;
+export const render = (ast, depth = 1) => {
+  const adjustDepthForBrackets = depth - 0.5;
   const result = ast.reduce((acc, item) => {
     if (item.type === 'nested') {
-      return `${acc}${addSpaces(item.depth)}  ${item.key}: ${render(item.value)}`;
+      return `${acc}${addSpaces(depth)}  ${item.key}: ${render(item.value, depth + 1)}`;
     }
-    return `${acc}${addSpaces(item.depth)}${renderTypeDispatch[item.type](item)}`;
+    return `${acc}${addSpaces(depth)}${renderTypeDispatch[item.type](item, depth)}`;
   }, '{\n');
   return `${result}${addSpaces(adjustDepthForBrackets)}}\n`;
 };
